ac1300a12de7217b7bdf93297688935c7c3e6a27
Oleg Dzhuraev <godlikeaurora@gmail.com>
2024-02-19 19:05:03 +0300

    Added several features, fixes and improvement (see description)
    Added support of AO atlas.
    Added support of Metallic workflow (Standard shader).
    Added possibility to change max atlas size for generated texture atlas.
    Added possibility to change generation folder.
    Added possibility to select, which texture atlases to bake.
    Fixed format for generated textures (Albedo, Specular).
    Fixed sRGB toggle for generated Specular textures.
    Added bake termination on some cases when wrong settings were set.
    Some other improvements and optimizations.

--- Editor/MeshBakerWindow.cs
+++ Editor/MeshBakerWindow.cs
@@ -25,41 +25,85 @@ namespace InsaneOne.DevTools
 	public sealed class MeshBakerWindow : EditorWindow
 	{
 		const int MaxUv = 2;
-		const int MaxAtlasSize = 2048;
+		const int MaxUniqueMeshes = 4;
+
 		const TextureFormat TexFormat = TextureFormat.RGBA32;
 		
 		static readonly int mainTexId = Shader.PropertyToID("_MainTex");
 		static readonly int specGlossMapId = Shader.PropertyToID("_SpecGlossMap");
+		static readonly int metallicGlossMapId = Shader.PropertyToID("_MetallicGlossMap");
 		static readonly int bumpMapId = Shader.PropertyToID("_BumpMap");
+		static readonly int aoMapId = Shader.PropertyToID("_OcclusionMap");
+
+		static readonly List<int> supportedAtlasSizes = new List<int>
+		{
+			512, 1024, 2048, 4096, 8192
+		};
 
 		readonly List<MeshFilter> toCombine = new List<MeshFilter>();
 		readonly List<MeshFilter> uniqueMeshFilters = new List<MeshFilter>();
 		readonly Dictionary<Mesh, Vector2[]> uniqueMeshesUvs = new Dictionary<Mesh, Vector2[]>();
 		readonly List<Material> uniqueMaterials = new List<Material>();
 
+		int selectedAtlasSizeId = 2;
+		
+		bool bakeNormals = true;
+		bool bakeSpecular = true;
+		bool bakeAo = false;
+		
 		MeshFilter finalMeshFilter;
 		bool disableOriginalMeshes;
-	
-		Texture2D tempNormalTex;
 
 		string bakePostfix = "00";
+		string bakeFolder = "Baked";
+
+		GUIStyle richLabelStyle;
+
+		bool isSpecularWorkflow;
 		
 		[MenuItem("Tools/Mesh Baker")]
 		public static void ShowWindow()
 		{
 			var wnd = GetWindow<MeshBakerWindow>();
 			wnd.titleContent = new GUIContent("Mesh Baker");
+			wnd.minSize = new Vector2(400, 400);
+
+			wnd.richLabelStyle = new GUIStyle(EditorStyles.label)
+			{
+				richText = true
+			};
 		}
 
 		public void OnGUI()
 		{
-			EditorGUILayout.HelpBox("Final mesh filter is filled automatically. But allows you to select MeshFilter component, where should be baken combined mesh.", MessageType.Info);
+			EditorGUILayout.HelpBox("This tool allows you to combine several meshes into one. Also, it combines their textures to one atlas for draw calls optimization.", MessageType.Info);
 			
+			GUILayout.Label("Scene settings", EditorStyles.boldLabel);
+			EditorGUILayout.HelpBox("Final mesh filter is filled automatically. But allows you to select MeshFilter component, where should be baken combined mesh.", MessageType.Info);
 			finalMeshFilter = EditorGUILayout.ObjectField("Final mesh filter", finalMeshFilter, typeof(MeshFilter), true) as MeshFilter;
 			disableOriginalMeshes = EditorGUILayout.Toggle("Disable original meshes", disableOriginalMeshes);
-
-			bakePostfix = SanitizeFilename(EditorGUILayout.TextField("Baked files postfix", bakePostfix));
 			
+			GUILayout.Label("Bake settings", EditorStyles.boldLabel);
+			
+			GUILayout.BeginHorizontal();
+			
+			GUILayout.Label($"Max final atlas size: <b>{GetAtlasSize()}</b>", richLabelStyle);
+			
+			if (GUILayout.Button("-"))
+				ChangeAtlasSize(false);
+			
+			if (GUILayout.Button("+"))
+				ChangeAtlasSize(true);
+			
+			GUILayout.EndHorizontal();
+			
+			bakeNormals = EditorGUILayout.Toggle("Normals atlas", bakeNormals);
+			bakeSpecular = EditorGUILayout.Toggle("Specular/Metallic atlas", bakeSpecular);
+			bakeAo = EditorGUILayout.Toggle("AO atlas", bakeAo);
+
+			bakeFolder = SanitizeFilename(EditorGUILayout.TextField("Bake folder name", bakeFolder));
+			bakePostfix = SanitizeFilename(EditorGUILayout.TextField("Baked files postfix", bakePostfix));
+		
 			var prevEnabled = GUI.enabled;
 
 			if (Selection.gameObjects.Length < 2)
@@ -68,11 +112,23 @@ namespace InsaneOne.DevTools
 				GUI.enabled = false;
 			}
 
-			if (GUILayout.Button("Bake meshes"))
+			if (GUILayout.Button("Bake"))
 				ProceedMeshes();
 
 			GUI.enabled = prevEnabled;
 		}
+
+		void ChangeAtlasSize(bool increase)
+		{
+			selectedAtlasSizeId += increase ? 1 : -1;
+
+			if (selectedAtlasSizeId < 0)
+				selectedAtlasSizeId = supportedAtlasSizes.Count - 1;
+			else if (selectedAtlasSizeId >= supportedAtlasSizes.Count)
+				selectedAtlasSizeId = 0;
+		}
+
+		int GetAtlasSize() => supportedAtlasSizes[selectedAtlasSizeId];
 
 		string SanitizeFilename(string input)
 		{
@@ -110,6 +166,15 @@ namespace InsaneOne.DevTools
 
 		void Prepare()
 		{
+			var path = GetPath();
+			path = path.Remove(path.Length - 1); // this is "/" symbol, to make check work correct we need to remove it
+			
+			if (!AssetDatabase.IsValidFolder(path))
+			{
+				AssetDatabase.CreateFolder("Assets", bakeFolder);
+				AssetDatabase.Refresh();
+			}
+			
 			foreach (var go in Selection.gameObjects)
 			{
 				if (!go.TryGetComponent<MeshFilter>(out var meshFilter))
@@ -126,6 +191,9 @@ namespace InsaneOne.DevTools
 					uniqueMeshesUvs.Add(meshFilter.sharedMesh, Array.Empty<Vector2>());
 				}
 			}
+			
+			if (uniqueMeshFilters.Count > MaxUniqueMeshes)
+				throw new IndexOutOfRangeException($"Maximum unique meshes is {MaxUniqueMeshes}! Bake process terminated.");
 		}
 
 		void ProceedUVs()
@@ -191,58 +259,84 @@ namespace InsaneOne.DevTools
 
 		void ProcessTextures()
 		{
-			var albedos = new Texture2D[uniqueMaterials.Count];
-			var speculars = new Texture2D[uniqueMaterials.Count];
-			var normals = new Texture2D[uniqueMaterials.Count];
-
-			for (var q = 0; q < uniqueMaterials.Count; q++)
+			var texturesCount = uniqueMaterials.Count;
+			var albedos = new Texture2D[texturesCount];
+			var speculars = bakeSpecular ? new Texture2D[texturesCount] : Array.Empty<Texture2D>();
+			var normals = bakeNormals ? new Texture2D[texturesCount] : Array.Empty<Texture2D>();
+			var aos = bakeAo ? new Texture2D[texturesCount] : Array.Empty<Texture2D>();
+			
+			isSpecularWorkflow = uniqueMaterials[0].GetTexture(specGlossMapId); // todo Mesh Baker - improve check, optimize it.
+			
+			for (var q = 0; q < texturesCount; q++)
 			{
 				var uniqueMaterial = uniqueMaterials[q];
 				albedos[q] = uniqueMaterial.GetTexture(mainTexId) as Texture2D;
-				speculars[q] = uniqueMaterial.GetTexture(specGlossMapId) as Texture2D;
 
+				if (albedos[q] == null)
+					throw new NullReferenceException("No Albedo texture in one of meshes materials! Bake process terminated.");
+				
 				var width = albedos[q].width;
 				var height = albedos[q].height;
+
+				if (bakeSpecular)
+				{
+					speculars[q] = uniqueMaterial.GetTexture(isSpecularWorkflow ? specGlossMapId : metallicGlossMapId) as Texture2D;
+
+					if (!speculars[q])
+						speculars[q] = CreateFilledTexture(width, height, new Color(0.5f, 0.5f, 0.5f, 1f)); // we need unique tex (when content still same) for unity texture packer - otherwise packed amount will be changed
+				}
+
+				if (bakeNormals)
+				{
+					var loadedNormal = uniqueMaterial.GetTexture(bumpMapId) as Texture2D;
+
+					if (loadedNormal)
+						normals[q] = RestoreNormal(loadedNormal);
+					else
+						normals[q] = CreateFilledTexture(width, height, new Color(0.5f, 0.5f, 1f, 1f));
+				}
 				
-				if (!speculars[q])
-					speculars[q] = CreateFilledTexture(width, height, new Color(0.5f, 0.5f, 0.5f, 1f)); // we need unique tex (when content still same) for unity texture packer - otherwise packed amount will be changed
+				if (bakeAo)
+				{
+					aos[q] = uniqueMaterial.GetTexture(aoMapId) as Texture2D;
 
-				var loadedNormal = uniqueMaterial.GetTexture(bumpMapId) as Texture2D;
-
-				if (loadedNormal)
-					normals[q] = RestoreNormal(loadedNormal);
-				else
-					normals[q] = CreateFilledTexture(width, height, new Color(0.5f, 0.5f, 1f, 1f)); 
+					if (!aos[q])
+						aos[q] = CreateFilledTexture(width, height, new Color(1f, 1f, 1f, 1f));
+				}
 			}
 
-			var finalAlbedo = MakeAtlas(albedos, "BakedAlbedo");
-			var finalSpecular = MakeAtlas(speculars, "BakedSpeculars");
-			var finalNormal = MakeAtlas(normals, "BakedNormals");
-
-			var finalMat = new Material(uniqueMaterials[0].shader);
-			finalMat.CopyPropertiesFromMaterial(uniqueMaterials[0]);
+			var finalMat = MakeMaterial(uniqueMaterials[0]);
 			
-			finalMat.SetTexture(mainTexId, finalAlbedo);
-			finalMat.SetTexture(specGlossMapId, finalSpecular);
-			finalMat.SetTexture(bumpMapId, finalNormal);
+			MakeAtlas(albedos, TextureType.Albedo, finalMat);
+			MakeAtlas(speculars, isSpecularWorkflow ? TextureType.Specular : TextureType.Metallic, finalMat);
+			MakeAtlas(normals, TextureType.Normal, finalMat);
+			MakeAtlas(aos, TextureType.AO, finalMat);
+		}
 
-			AssetDatabase.CreateAsset(finalMat, $"{GetPath()}BakedMaterial{bakePostfix}.mat");
+		Material MakeMaterial(Material originalMat)
+		{
+			var material = new Material(originalMat.shader);
+			material.CopyPropertiesFromMaterial(originalMat);
+			
+			if (bakeNormals)
+				material.EnableKeyword("_NORMALMAP");
+			
+			if (bakeSpecular)
+				material.EnableKeyword("_SPECGLOSSMAP");
+			
+			AssetDatabase.CreateAsset(material, $"{GetPath()}BakedMaterial{bakePostfix}.mat");
 			AssetDatabase.SaveAssets();
 			AssetDatabase.Refresh();
-			
-			var meshRend = finalMeshFilter.GetComponent<MeshRenderer>();
-			meshRend.sharedMaterial = finalMat;
 
-			finalMat.EnableKeyword("_NORMALMAP");
-			finalMat.EnableKeyword("_SPECGLOSSMAP");
+			var meshRend = finalMeshFilter.GetComponent<MeshRenderer>();
+			meshRend.sharedMaterial = material;
+			
+			return material;
 		}
 		
 		string GetPath(bool includeAssets = true)
 		{
-			var path = "Assets/Baked/";
-
-			if (!AssetDatabase.IsValidFolder(path))
-				AssetDatabase.CreateFolder("Assets", "Baked");
+			var path = $"Assets/{bakeFolder}/";
 
 			if (!includeAssets)
 				path = path.Replace("Assets", "");
@@ -250,26 +344,40 @@ namespace InsaneOne.DevTools
 			return path;
 		}
 		
-		Texture2D MakeAtlas(Texture2D[] textures, string filename)
+		Texture2D MakeAtlas(Texture2D[] textures, TextureType type, Material applyMaterial)
 		{
-			var texture = new Texture2D(MaxAtlasSize, MaxAtlasSize, TexFormat, true);
-			texture.PackTextures(textures, 0, MaxAtlasSize);
+			if (!IsNeedBake(type))
+				return null;
+
+			var atlasSize = GetAtlasSize();
+			var texture = new Texture2D(atlasSize, atlasSize, TexFormat, true);
+			texture.PackTextures(textures, 0, atlasSize);
 
 			var bytes = texture.EncodeToPNG();
-			var fileName = $"{filename}{bakePostfix}.png";
+			var fileName = $"Baked{type}{bakePostfix}.png";
 			
 			File.WriteAllBytes(Application.dataPath + GetPath(false) +  fileName, bytes);
 			AssetDatabase.Refresh();
 
-			if (filename.Contains("Normal"))
+			var importer = AssetImporter.GetAtPath(GetPath() + fileName) as TextureImporter;
+
+			if (importer)
 			{
-				var importer = TextureImporter.GetAtPath(GetPath() + fileName) as TextureImporter;
-				importer.textureType = TextureImporterType.NormalMap;
+				importer.textureType = type == TextureType.Normal
+					? TextureImporterType.NormalMap
+					: TextureImporterType.Default;
+
+				if (type == TextureType.Metallic || type == TextureType.Specular)
+					importer.sRGBTexture = false;
+				
 				importer.SaveAndReimport();
 			}
 
 			var finalTex = AssetDatabase.LoadAssetAtPath<Texture2D>(GetPath() + fileName);
-			
+
+			if (applyMaterial)
+				applyMaterial.SetTexture(GetMaterialId(type), finalTex);
+
 			return finalTex;
 		}
 
@@ -303,5 +411,33 @@ namespace InsaneOne.DevTools
 
 			return tex;
 		}
+
+		bool IsNeedBake(TextureType type)
+		{
+			return type switch
+			{
+				TextureType.Albedo => true,
+				TextureType.Specular => bakeSpecular,
+				TextureType.Metallic => bakeSpecular,
+				TextureType.Normal => bakeNormals,
+				TextureType.AO => bakeAo,
+				_ => throw new ArgumentOutOfRangeException(nameof(type), type, null)
+			};
+		}
+		
+		int GetMaterialId(TextureType type)
+		{
+			return type switch
+			{
+				TextureType.Albedo => mainTexId,
+				TextureType.Specular => specGlossMapId,
+				TextureType.Metallic => metallicGlossMapId,
+				TextureType.Normal => bumpMapId,
+				TextureType.AO => aoMapId,
+				_ => throw new ArgumentOutOfRangeException(nameof(type), type, null)
+			};
+		}
 	}
+
+	enum TextureType { Albedo, Normal, Specular, Metallic, AO }
 }
